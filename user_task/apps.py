7b436a9cc9fe5b7d70eb2505a9abf0d3c2aba5e3
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-07-14 18:35:48 +0600

    create app user_task

--- user_task/apps.py
+++ user_task/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsertaskConfig(AppConfig):
+class UserTaskConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usertask'
+    name = 'user_task'
