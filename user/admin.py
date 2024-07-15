9f1e697c66d947c45d326e3e2cb235a325995707
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-07-15 17:31:52 +0600

    update admin with forms and custom user

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserAdminCreationForm, UserAdminChangeForm
 
-# Register your models here.
+User = get_user_model()
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    list_display = ['email', 'is_admin']
+    list_filter = ['is_admin']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ("first_name", "middle_name", "last_name", "phone_number")}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'password_2')}
+        ),
+    )
+
+    search_fields = ['email']
+    ordering = ['email']
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
