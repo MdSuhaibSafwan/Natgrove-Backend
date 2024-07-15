2c95e8b900956b8b361e0d9020588b20218c7ca5
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-07-15 17:32:06 +0600

    add relevant fields in user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+# django.contrib.auth.models
+
+from django.contrib.auth.models import PermissionsMixin, BaseUserManager
+from django.contrib.auth.base_user import AbstractBaseUser
+
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password):
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, password):
+        user = self._create_user(email, password)
+        # user.is_superuser = True
+        user.is_active = True
+        # user.is_admin = True
+        user.save()
+        return user		
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_active = True
+        user.is_admin = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, )
+    first_name = models.CharField(max_length=100, null=True)
+    middle_name = models.CharField(max_length=100, null=True)
+    last_name = models.CharField(max_length=100, null=True)
+    address = models.TextField(null=True)
+    phone_number = models.CharField(max_length=16, null=True)
+
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELD = "email"
+    EMAIL_FIELD = "email"
+
+    def __str__(self):
+        return self.email
+
+    objects = UserManager()
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+
+
+
