0d1ae6a28accc220bdde085783d1210fcc992b1e
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-07-31 23:08:16 +0600

    changed name officially

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClimateOClockBackend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Natgrove.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
