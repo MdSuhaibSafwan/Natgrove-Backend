7c94d157e801138c8afb0df5b47f531550566023
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-10-17 02:27:36 +0300

    update natgrove settings

--- Natgrove/settings.py
+++ Natgrove/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "1234")
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "natgrove-47db19e790a0.herokuapp.com", "natgrove.com", "www.natgrove.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "natgrove-47db19e790a0.herokuapp.com", "natgrove.com", "www.natgrove.com", '.vercel.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
