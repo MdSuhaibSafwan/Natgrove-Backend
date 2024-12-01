049b71a78c613846b18fce1a021452ee4ff2429e
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-12-01 12:40:47 +0300

    update settings

--- Natgrove/settings.py
+++ Natgrove/settings.py
@@ -115,7 +115,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
 
 AUTH_PASSWORD_VALIDATORS = [
